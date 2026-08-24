c7c4c5a2279ba5fdd4a877783c59bc7840549fd6
Jeff Repanich <jrepanich@gmail.com>
2026-08-24 01:42:23 -0400

    Complete manifest read snapshots

--- src/Pants/Storage/Internal/Manifest/ManifestReadSnapshot.cs
+++ src/Pants/Storage/Internal/Manifest/ManifestReadSnapshot.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace Cntryl.Pants.Storage.Internal.Manifest;
 
 sealed record ManifestReadSnapshot(
@@ -5,12 +7,23 @@ sealed record ManifestReadSnapshot(
     ulong NextWalSequence,
     IReadOnlyDictionary<uint, ulong> NextSstSequences,
     IReadOnlyList<FileMeta> Files,
-    IReadOnlyList<ColumnFamilyMeta> ColumnFamilies)
+    IReadOnlyList<ColumnFamilyMeta> ColumnFamilies,
+    object? CloudCheckpoint,
+    ulong EditCheckpointId)
 {
     public static ManifestReadSnapshot Create(ManifestState manifest) => new(
         manifest.LastPersistedSequence,
         manifest.NextWalSeq,
         new Dictionary<uint, ulong>(manifest.NextSstSeqs),
         manifest.Files.Select(static file => file.Clone()).ToArray(),
-        manifest.ColumnFamilies.Select(static family => family.Clone()).ToArray());
+        manifest.ColumnFamilies.Select(static family => family.Clone()).ToArray(),
+        CloneCloudCheckpoint(manifest.CloudCheckpoint),
+        manifest.EditCheckpointId);
+
+    static object? CloneCloudCheckpoint(object? checkpoint) => checkpoint switch
+    {
+        null => null,
+        JsonElement element => element.Clone(),
+        _ => JsonSerializer.SerializeToElement(checkpoint)
+    };
 }
